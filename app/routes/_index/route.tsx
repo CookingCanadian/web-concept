6cd876e55099448ad0e608bb457968d0f4b2ddf2
CookingCanadian <cookedcanadian@gmail.com>
2024-12-23 12:26:38 -0500

    first page

--- app/routes/_index/route.tsx
+++ app/routes/_index/route.tsx
@@ -1,6 +1,11 @@
 import { LinksFunction, LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
 import { getUrlOriginWithPath } from '~/utils';
 import styles from './_index.module.scss';
+import styles0 from './route.module.scss';
+import OfficeIconPng from '../../../src/assets/office_icon.png';
+import SchoolIconPng from '../../../src/assets/school_icon.png';
+import HotelIconPng from '../../../src/assets/hotel_icon.png';
+import GymIconCopy1Png from '../../../src/assets/gym_icon_copy1.png';
 
 export const loader = ({ request }: LoaderFunctionArgs) => {
     return { canonicalUrl: getUrlOriginWithPath(request.url) };
@@ -9,19 +14,55 @@ export const loader = ({ request }: LoaderFunctionArgs) => {
 export default function HomePage() {
     return (
         <div className={styles.root}>
-            <h2 className={styles.title}>Welcome To App Homepage 🎉</h2>
-            <span className={styles.paragraph}>
-                Drag here elements from the Add Elements Panel
-                <br /> and style them using the Styles panel
-            </span>
+            <img
+                src="https://reverttechnologies.com/cdn/shop/files/b2.png?v=1730099475"
+                alt=""
+                className={styles0.bannerImage}
+            />
+            <div className={styles0.contentContainer}>
+                <h1 className={styles0.pageTitle}>Select Segment</h1>
+                <div className={styles0.segmentContainer}>
+                    <div className={styles0.segmentBlock}>
+                        <img src={GymIconCopy1Png} alt="" className={styles0.segmentImage} />
+                        <span className={styles0.segmentName}>Gym</span>
+                    </div>
+                    <div className={styles0.segmentBlock}>
+                        <img src={OfficeIconPng} alt="" className={styles0.segmentImage} />
+                        <span className={styles0.segmentName}>Office</span>
+                    </div>
+                    <div className={styles0.segmentBlock}>
+                        <img src={SchoolIconPng} alt="" className={styles0.segmentImage} />
+                        <span className={styles0.segmentName}>School</span>
+                    </div>
+                    <div className={styles0.segmentBlock}>
+                        <img src={HotelIconPng} alt="" className={styles0.segmentImage} />
+                        <span className={styles0.segmentName}>Hotel</span>
+                    </div>
+                </div>
+                <div className={styles0.navigationBar}>
+                    <button className={styles0.backButton}>Back</button>
+                    <div className={styles0.progressCircle}>
+                        <span className={styles0.span1}>Segment</span>
+                    </div>
+                    <div className={styles0.separatorLine} />
+                    <div className={styles0.progressCircle}>
+                        <span className={styles0.span1}>Quantity</span>
+                    </div>
+                    <div className={styles0.separatorLine} />
+                    <div className={styles0.progressCircle}>
+                        <span className={styles0.span1}>Location</span>
+                    </div>
+                    <button className={styles0.nextbutton}>Next</button>
+                </div>
+            </div>
         </div>
     );
 }
 
 export const meta: MetaFunction<typeof loader> = ({ data }) => {
-    const title = 'Blank Starter';
-    const description = 'Welcome to the Blank Starter';
-    const imageUrl = 'https://website-starter.com/og-image.png';
+    const title = 'Web Concept - V2';
+    const description = 'Revert Technologies';
+    const imageUrl = 'https://reverttechnologies.com/cdn/shop/files/RVT-Hor-Logo-Neg_240x.png';
 
     return [
         { title },
